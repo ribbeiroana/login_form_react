d77db41ea27706e4ad786a6373791290f712f2d1
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-10-30 10:38:13 -0300

    login styles

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LoginForm from './components/LoginForm/loginForm.tsx';
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
